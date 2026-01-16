41c0ebcdb6f6e15134e9f4330929612eef326866
Manan Jain <174644789+MananJain39@users.noreply.github.com>
2026-01-16 06:33:47 +0530

    test(tracker)

--- dev_test/tracker_test.py
+++ dev_test/tracker_test.py
@@ -2,6 +2,8 @@ from Server.services.tracker import Tracker
 
 t = Tracker()
 e1 = t.get_or_create_experiment("exp1")
-e2 = t.get_or_create_experiment("exp2")
+# e2 = t.get_or_create_experiment("exp2")
+run = t.start_run(e1.id)
 
-print(e1.id == e2.id)  # Should be False
+
+print(run.id, run.status)  
