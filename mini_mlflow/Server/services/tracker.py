793f9508157c1bf5e859946447bc7d64e9bee8de
Manan Jain <174644789+MananJain39@users.noreply.github.com>
2026-03-24 04:32:09 +0530

    fix(tracker): change `sessionlocal` to `db.session.sessionlocal`

--- mini_mlflow/Server/services/tracker.py
+++ mini_mlflow/Server/services/tracker.py
@@ -1,13 +1,13 @@
 from datetime import datetime, timezone
 from Server.DB.models import Experiment, Run, Param, Metric, Artifact
-from Server.DB.session import SessionLocal
+from Server.DB import session as db_session
 import os
 import shutil
 
 class Tracker:
 
     def get_or_create_experiment(self, name: str):
-        db = SessionLocal() # create a new session
+        db = db_session.SessionLocal() # create a new session
         try:
             exp = db.query(Experiment).filter_by(name = name).first()
             if exp:
@@ -22,7 +22,7 @@ class Tracker:
             db.close()
 
     def start_run(self, experiment_id:str):
-        db = SessionLocal()
+        db = db_session.SessionLocal()
         run = Run(experiment_id = experiment_id)
         db.add(run)
         db.commit()
@@ -31,7 +31,7 @@ class Tracker:
         return run
     
     def end_run(self, run_id:str):
-        db = SessionLocal()
+        db = db_session.SessionLocal()
         run = db.get(Run, run_id)
         if not run:
             raise ValueError(f"Run not found: {run_id}")
@@ -43,7 +43,7 @@ class Tracker:
         return run
 
     def log_param(self, run_id:str, key:str, value:str):
-        db = SessionLocal()
+        db = db_session.SessionLocal()
 
         run = db.get(Run, run_id)
         if not run:
@@ -57,7 +57,7 @@ class Tracker:
         return param
 
     def log_metric(self, run_id:str, key:str, value:float, step: int = 0):
-        db = SessionLocal()
+        db = db_session.SessionLocal()
         
         run = db.get(Run, run_id)
         if not run:
@@ -72,7 +72,7 @@ class Tracker:
 
     ARTIFACT_ROOT = "storage/artifacts"
     def log_artifact(self, run_id: str, file_path: str):
-        db = SessionLocal()
+        db = db_session.SessionLocal()
         run = db.get(Run, run_id)
         if not run:
             raise ValueError(f"Run not found: {run_id}")
